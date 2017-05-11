00eeb381a575926d14b384134abdebfa95ef83ce
Justin Boucher <jboucher@splunk.com>
2017-05-11 09:01:39 -0700

    Modified framework with full URI strings for portability

--- bin/access_generator.py
+++ bin/access_generator.py
@@ -9,15 +9,27 @@ import api_framework
 import json
 import os
 import cherrypy, webbrowser
+import ConfigParser
 
 # Setup Splunk Environment
-APPNAME = '<APPNAME>'
-CONFIG = '/bin/user_settings.txt'
+APPNAME = 'test_app'
+PARSER_CONFIG = 'appconfig.conf'
+CONFIG = '/bin/token_settings.txt'
 SPLUNK_HOME = os.environ['SPLUNK_HOME']
+
+parser = ConfigParser.SafeConfigParser()
 
 tokenfile = SPLUNK_HOME + '/etc/apps/' + APPNAME + CONFIG
 
 api = api_framework.APIFramework()
+
+# All information must be as on the API configuration page for the app you are connecting to.
+    # Load Settings
+parser.read(SPLUNK_HOME + '/etc/apps/' + APPNAME + '/local/' + PARSER_CONFIG)
+if parser.has_section('Authentication'):
+    pass
+else:
+    parser.read(SPLUNK_HOME + '/etc/apps/' + APPNAME + '/default/' + PARSER_CONFIG)
 
 # Try to read existing token pair
 try:
@@ -36,12 +48,18 @@ except IOError:
     json.dump(token, open(tokenfile, 'w'))
 
 # Sample API call
-response = api.api_call(token, '/1/user/-/profile.json')
+SAMPLE_CALL = parser.get('AppServer', 'SAMPLE_CALL')
+response = api.api_call(token, SAMPLE_CALL)
 
 # Token is part of the response. Note that the token pair can change when a refresh is necessary.
 # So we replace the current token with the response one and save it.
 token = response['token']
-json.dump(token, open(tokenfile, 'w'))
-
-# Do something with the response
-print "Welcome %s, your %s account is now connected!" % (response['user']['displayName'], APPNAME)
+try:
+    json.dump(token, open(tokenfile, 'w'))
+    # Do something with the response
+    print "\nHere was the response:"
+    print response
+    print "\n\nCongrats your %s account is now connected! Look above for the response.\n" % APPNAME
+except:
+    print "There was an error retrieving the sample data call. \
+    Please review the %s configuration and your API documentation." % APPNAME
